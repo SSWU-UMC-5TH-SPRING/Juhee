c4db35c10fda95235c4b0fec6c48dd08816a21f8
jhsong76 <20190976@sungshin.ac.kr>
2023-11-06 01:19:20 +0900

    feat: review 도메인

---
+++ src/main/java/umc/spring/domain/Review.java
@@ -0,0 +1,27 @@
+package umc.spring.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.spring.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewIdx;
+    private String content;
+    private String reviewImg;
+    private Integer rating;
+}
