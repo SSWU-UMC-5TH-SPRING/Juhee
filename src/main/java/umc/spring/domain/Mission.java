17404dfc539214362796f67826f0bfbb4d66a3a0
jhsong76 <20190976@sungshin.ac.kr>
2023-11-06 01:09:31 +0900

    feat: mission 도메인

---
+++ src/main/java/umc/spring/domain/Mission.java
@@ -0,0 +1,35 @@
+package umc.spring.domain;
+
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.spring.domain.common.BaseEntity;
+import umc.spring.domain.enums.MissionStatus;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Mission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long missionIdx;
+
+    private Integer price;
+    private Integer rewardPoints;
+    private LocalDate deadline;
+    @Enumerated(EnumType.STRING)
+    private MissionStatus status;
+
+}
