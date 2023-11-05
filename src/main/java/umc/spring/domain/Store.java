8976c25d77d5d6ea2d33c763338681d3c820676d
jhsong76 <20190976@sungshin.ac.kr>
2023-11-06 01:06:27 +0900

    feat: store 도메인

---
+++ src/main/java/umc/spring/domain/Store.java
@@ -0,0 +1,33 @@
+package umc.spring.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.spring.domain.common.BaseEntity;
+import umc.spring.domain.enums.StoreCategory;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long storeIdx;
+
+    @Enumerated(EnumType.STRING)
+    private StoreCategory category;
+
+    private String location;
+    private String region;
+}
