712bd97770c9c53d57bab55636ffe134a493f624
jhsong76 <20190976@sungshin.ac.kr>
2023-11-06 01:24:08 +0900

    feat: replies 도메인

---
+++ src/main/java/umc/spring/domain/Replies.java
@@ -0,0 +1,23 @@
+package umc.spring.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Replies {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long repliesIdx;
+    private String content;
+}
